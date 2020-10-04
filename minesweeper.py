ca24bfd539a13312293ef2329f253b71d55935ea
Max Bergmark <max.bergmark@gmail.com>
2020-10-04 12:13:38 +0200

    Renamed bombs to mines

--- minesweeper.py
+++ minesweeper.py
@@ -12,10 +12,10 @@ class GameState(Enum):
 
 class MineSweeper:
 
-	def __init__(self, xdim, ydim, n_bombs):
+	def __init__(self, xdim, ydim, n_mines):
 		self.xdim = xdim
 		self.ydim = ydim
-		self.n_bombs = min(n_bombs, xdim * ydim-1)
+		self.n_mines = min(n_mines, xdim * ydim-1)
 		self.upscale = 1000 // self.xdim
 		self.slowest_refresh = 4.0
 		self.fastest_refresh = 1.0
@@ -50,7 +50,7 @@ class MineSweeper:
 
 	def calculate_refresh_time(self):
 		dt = self.slowest_refresh - self.fastest_refresh
-		p = len(self.correct_markings) / self.n_bombs
+		p = len(self.correct_markings) / self.n_mines
 		self.refresh_time = self.slowest_refresh - dt * p
 
 	def resweep(self):
@@ -59,11 +59,11 @@ class MineSweeper:
 			for _ in range(self.ydim)]
 		self.revealed = [[False for _ in range(self.xdim)] 
 			for _ in range(self.ydim)]
-		self.marked_bombs = [[False for _ in range(self.xdim)] 
+		self.marked_mines = [[False for _ in range(self.xdim)] 
 			for _ in range(self.ydim)]
-		self.place_bombs()
+		self.place_mines()
 		for px, py in self.markings:
-			self.marked_bombs[py][px] = True
+			self.marked_mines[py][px] = True
 		for px, py in self.clicks:
 			self.reveal(px, py)
 		self.last_resweep = time.time()
@@ -71,14 +71,14 @@ class MineSweeper:
 		self.draw_board()
 		t1 = time.time()
 
-	def place_bombs(self):
+	def place_mines(self):
 		for bx, by in self.correct_markings:
 			self.board[by][bx] = -1
 			for x, y in self.get_neighbors(bx, by):
 				if self.board[y][x] != -1:
 					self.board[y][x] += 1
 
-		for _ in range(self.n_bombs - len(self.correct_markings)):
+		for _ in range(self.n_mines - len(self.correct_markings)):
 			while True:
 				rx = random.randint(0, self.xdim-1)
 				ry = random.randint(0, self.ydim-1)
@@ -152,7 +152,7 @@ class MineSweeper:
 
 	def draw_text(self, row, col):
 		text_pos = (int(self.upscale*(col+.3)),int(self.upscale*(row+.8)))
-		if not self.marked_bombs[row][col]:
+		if not self.marked_mines[row][col]:
 			if self.board[row][col] > 0 and self.revealed[row][col]:
 				s = str(self.board[row][col])
 				self.put_text(*text_pos, s, (0, 255, 0))
@@ -171,7 +171,7 @@ class MineSweeper:
 		for row in range(self.ydim):
 			for col in range(self.xdim):
 				self.draw_background(row, col)
-				if self.marked_bombs[row][col] or self.revealed[row][col]:
+				if self.marked_mines[row][col] or self.revealed[row][col]:
 					self.draw_text(row, col)
 				if (col, row) in self.clicks:
 					self.draw_click(row, col)
@@ -181,10 +181,10 @@ class MineSweeper:
 	def draw_stats(self):
 		self.canvas[self.upscale*self.ydim:,:,:] = 0
 		y_pos = int(self.upscale*(self.ydim+.8))
-		bomb_pos = int(self.upscale*(2+.2))
+		mine_pos = int(self.upscale*(2+.2))
 		click_pos = int(self.upscale*(7+.2))
-		n_bombs = self.n_bombs - len(self.markings)
-		self.put_text(bomb_pos, y_pos, "B: %d" % n_bombs, (0, 0, 255))
+		n_mines = self.n_mines - len(self.markings)
+		self.put_text(mine_pos, y_pos, "B: %d" % n_mines, (0, 0, 255))
 		clicks_str = "C: %d" % len(self.clicks)
 		self.put_text(click_pos, y_pos, clicks_str, (255, 255, 255))
 
@@ -226,6 +226,7 @@ class MineSweeper:
 		while self.game_state == GameState.LOSS or self.game_state == GameState.WIN:
 			cv2.imshow(self.window_name,self.canvas)
 			if cv2.waitKey(10) & 0xFF == ord("q"):
+				self.game_state = GameState.EXIT
 				break
 
 	def left_click(self, cx, cy):
@@ -236,12 +237,12 @@ class MineSweeper:
 			self.replay_game()
 
 	def check_win(self):
-		if len(self.correct_markings) == len(self.markings) == self.n_bombs:
+		if len(self.correct_markings) == len(self.markings) == self.n_mines:
 			self.game_state = GameState.WIN
 
-	def mark_bomb(self, cx, cy):
-		if not self.revealed[cy][cx] and len(self.markings) < self.n_bombs:
-			self.marked_bombs[cy][cx] = True
+	def mark_mine(self, cx, cy):
+		if not self.revealed[cy][cx] and len(self.markings) < self.n_mines:
+			self.marked_mines[cy][cx] = True
 			self.markings.add((cx, cy))
 			if self.board[cy][cx] == -1:
 				self.correct_markings.add((cx, cy))
@@ -251,8 +252,8 @@ class MineSweeper:
 			# print("mark", len(self.correct_markings), len(self.markings))
 			self.check_win()
 
-	def unmark_bomb(self, cx, cy):
-		self.marked_bombs[cy][cx] = False
+	def unmark_mine(self, cx, cy):
+		self.marked_mines[cy][cx] = False
 		self.markings.remove((cx, cy))
 		if self.board[cy][cx] == -1:
 			self.correct_markings.remove((cx, cy))
@@ -260,10 +261,10 @@ class MineSweeper:
 		self.check_win()
 
 	def right_click(self, cx, cy):
-		if not self.marked_bombs[cy][cx]:
-			self.mark_bomb(cx, cy)
+		if not self.marked_mines[cy][cx]:
+			self.mark_mine(cx, cy)
 		else:
-			self.unmark_bomb(cx, cy)
+			self.unmark_mine(cx, cy)
 
 	def on_mouse(self, event, x, y, flags, param):
 		cx = x // self.upscale
@@ -278,4 +279,5 @@ class MineSweeper:
 
 game = MineSweeper(15, 15, 20)
 while game.game_state != GameState.EXIT:
+	print("test", game.game_state)
 	game.main_loop()
